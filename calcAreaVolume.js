548997e21956f7ebf6e592e5dc75936afd336c9f
pabloaredu <pablo.adu83@gmail.com>
2018-01-19 02:52:23 +0000

    adding the 3 closure exercises

---
+++ calcAreaVolume.js
@@ -0,0 +1,23 @@
+var wrapLog = function (cb, name) {
+  const transformer = cb;
+  const name2 = name;
+  return function (a,b,c,d,e) {
+    console.log(a,b,c,d,e);
+  }
+};
+
+var area = function (x, y) {
+  return x * y;
+};
+var logArea = wrapLog(area, "area");
+
+logArea(5, 3); // area(5, 3) => 15
+logArea(3, 2); // area(3, 2) => 6
+
+var volume = function (x, y, z) {
+  return x * y * z;
+};
+var logVolume = wrapLog(volume, "volume");
+
+logVolume(5, 3, 2); // volume(5, 3, 2) => 30
+logVolume(3, 2, 4); // volume(3, 2, 4) => 24
---
+++ countDownGenerator.js
@@ -0,0 +1,15 @@
+var countdownGenerator = function (x) {
+  y = x+1;
+  return function(){
+    y -= 1
+    return y;
+    }
+};
+
+var countdown = countdownGenerator(3);
+console.log(countdown()); // T-minus 3...
+console.log(countdown()); // T-minus 2...
+console.log(countdown()); // T-minus 1...
+console.log(countdown()); // Blast Off!
+console.log(countdown()); // Rockets already gone, bub!
+console.log(countdown()); // Rockets already gone, bub!
